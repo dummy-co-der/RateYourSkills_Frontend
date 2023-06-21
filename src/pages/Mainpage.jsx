4f99a4222c5142d806f894bf1c16dbc58e495902
dummy-co-der <maheshwarianuj1001@gmail.com>
2023-06-21 13:27:12 +0530

    common folder

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,9 +1,9 @@
 import Banner from "../components/mainpage/banner/Banner";
 import Video from "../components/mainpage/video/Video";
 import Benefit from "../components/mainpage/benefit/Benefit";
-import Footer from "../components/mainpage/footer/Footer";
 // import Faq from "../components/mainpage/faq/Faq";
 import Carousel from "../components/mainpage/carousel/Carousel";
+import Footer from "../common/footer/Footer";
 
 function Mainpage() {
   return (

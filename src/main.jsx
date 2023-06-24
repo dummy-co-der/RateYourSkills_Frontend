cd2fb03b99b654d9f16bed0c685f0c2607cf7cd0
dummy-co-der <maheshwarianuj1001@gmail.com>
2023-06-24 18:59:50 +0530

    added routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import App from "./App.jsx";
 import "./index.css";
 import store from "./authentication/store.jsx";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </Provider>
 );
